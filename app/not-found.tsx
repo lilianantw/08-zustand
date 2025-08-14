589c70fdf70bd5a15a71bc899a519f34c01647b2
liliana_toiunda <liliana.ntw@gmail.com>
2025-08-14 09:56:33 +0300

    not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,9 @@
 // app/not-found.tsx
 import type { Metadata } from "next";
+import Link from "next/link";
 import css from "./not-found.module.css";
 
+// SEO метаданные
 export const metadata: Metadata = {
   title: "404 - Page Not Found | NoteHub",
   description: "The page you are looking for does not exist on NoteHub.",
@@ -9,7 +11,7 @@ export const metadata: Metadata = {
     title: "404 - Page Not Found | NoteHub",
     description: "The page you are looking for does not exist on NoteHub.",
     siteName: "NoteHub",
-    url: "https://notehub.vercel.app/not-found",
+    url: "https://08-zustand-ten-lake.vercel.app/not-found",
     images: [
       {
         url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
@@ -22,13 +24,17 @@ export const metadata: Metadata = {
   },
 };
 
+// Серверный компонент Not Found
 export default function NotFound() {
   return (
-    <div>
+    <div className={css.wrapper}>
       <h1 className={css.title}>404 - Page not found</h1>
       <p className={css.description}>
         Sorry, the page you are looking for does not exist.
       </p>
+      <Link href="/" className={css.link}>
+        Go back to home
+      </Link>
     </div>
   );
 }
