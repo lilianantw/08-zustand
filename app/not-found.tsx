7b48545b79e304528f31d55f23d9b0fc8d814f76
liliana_toiunda <liliana.ntw@gmail.com>
2025-08-14 09:51:51 +0300

    fix not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,7 @@
-"use client";
-
-import { Metadata } from "next";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+// app/not-found.tsx
+import type { Metadata } from "next";
 import css from "./not-found.module.css";
 
-// Метаданные для страницы 404
 export const metadata: Metadata = {
   title: "404 - Page Not Found | NoteHub",
   description: "The page you are looking for does not exist on NoteHub.",
@@ -13,7 +9,7 @@ export const metadata: Metadata = {
     title: "404 - Page Not Found | NoteHub",
     description: "The page you are looking for does not exist on NoteHub.",
     siteName: "NoteHub",
-    url: "https://08-zustand-ten-lake.vercel.app/not-found",
+    url: "https://notehub.vercel.app/not-found",
     images: [
       {
         url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
@@ -26,15 +22,7 @@ export const metadata: Metadata = {
   },
 };
 
-// Компонент страницы 404
-const NotFound = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => router.push("/"), 3000);
-    return () => clearTimeout(timer);
-  }, [router]);
-
+export default function NotFound() {
   return (
     <div>
       <h1 className={css.title}>404 - Page not found</h1>
@@ -43,6 +31,4 @@ const NotFound = () => {
       </p>
     </div>
   );
-};
-
-export default NotFound;
+}
